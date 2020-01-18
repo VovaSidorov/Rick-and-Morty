db478bb5c6767288ec66d4e2bbb306abc91769b5
VovaSidorov <you@example.com>
2020-01-18 22:36:49 +0200

    add validation for Catalog

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import CharacterCard from '../CharacterCard'
+import PropTypes from 'prop-types';
+import assignPropTypes from 'assign-prop-types';
 
 import './style.css';
 
-export default ({characters}) => {
+export default assignPropTypes({
+    characters: PropTypes.array.isRequired,
+})(({characters}) => {
     return (
         <section className="Home__ShowcaseWrapper">
             <div className="Home__ShowcaseInner">
+
                 {
                     characters.map(el => (<CharacterCard {...el} key={el.id} />))
                 }
+
             </div>
         </section>
     );
-}
+})
